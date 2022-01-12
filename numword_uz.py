a5fd51a2298b80886e60a76350b111fc85a481c4
Baxtiyor Eshametov <52046670+baxt1yor@users.noreply.github.com>
2022-01-12 16:27:43 +0500

    Update numword_uz.py

--- numword_uz.py
+++ numword_uz.py
@@ -1,6 +1,6 @@
+import random
 number_dict = {
-    0:"no'l",
-    1: "bir",
+    1:"bir",
     2:"ikki",
     3:"uch",
     4:"to'rt",
@@ -21,34 +21,29 @@ number_dict = {
 }
 
 denominations = {
+    1000000000000:"trilion",
     1000000000:"milliard",
     1000000:"million",
     1000:"ming",
-    100:"yuz",
+    100:"yuz"
 }
 
 def get_len(num):
         return len(str(num))
 
-def convert(num, rec = False):
-        if not rec:
-            return number_dict[0]
+def convert(num):
         if num:
             if get_len(num) == 1:
-                    return number_dict[num % 10]
-
+                    return f"{number_dict[num % 10]}"
             elif get_len(num) == 2:
-                return number_dict[num // 10 * 10] + " " + convert(num % 10, True)
-
+                return f"{number_dict[num // 10 * 10]} {str(convert(num % 10)).strip()}"
             else:
                 for d in denominations:
-                    if num not in [100, 1000]:
-                        res = num // d
-                        if res:
-                            return f"{convert(res, True)} {denominations[d]} {convert(num%d, True)}"
-                    else:
-                        return f"{denominations[num]}"
+                    res = num // d
+                    if res:
+                        return f"{str(convert(res)).strip()} {denominations[d]} {str(convert(num%d)).strip()}"
         return ""
 
 num = int(input())
-print(convert(num, num != 0))
+
+print(convert(num))
